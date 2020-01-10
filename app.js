8819bfa34c6d2164bce442a8cb626219e37c2203
Daniela Galledi <dgalleguillos@utem.cl>
2020-01-10 13:09:15 -0300

    ADD: Starting server

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// funcionando 
+// funcionando
 //
 //docker build -t node-api .
 //docker run -it -p 9001:3000 -v "$(pwd):/app" node-api
@@ -12,13 +12,13 @@ const get = require('./utils/get');
 
 const getInfo = async (direccion) => {
   try{
-      const dir = await get.getCoordenadas(direccion);   
+      const dir = await get.getCoordenadas(direccion);
 
       const lat = await dir.geolocation.latitude;
       const long = await dir.geolocation.longitude;
       console.log(dir);
       console.log(`la latitud es ${lat} y la longitud es ${long}`);
-      
+
     } catch (e) {
       console.log(`No se pudo obtener informacion de ${direccion}`)
     }
@@ -27,3 +27,13 @@ const getInfo = async (direccion) => {
 getInfo('palestrina 2710, san joaquin');
 
 
+// settings
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send(`Hello jiji!`));
+
+// starting the server
+app.listen(app.get('port'), () => {
+  console.log(`Server on port ${port}`);
+
+});
