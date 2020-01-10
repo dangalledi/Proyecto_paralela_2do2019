8821fdff932897c5b70586c4f6be6d20cad7d9db
Daniela Galledi <dangalledi@gmail.com>
2020-01-10 19:03:50 -0300

    ADD: Validacion, limpieza de codigo.

--- utils/get.js
+++ utils/get.js
@@ -3,29 +3,24 @@ const axios = require('axios');
 
 const getCoordenadas = async (direccion) =>{
     direccion = encodeURI(direccion);
-    console.log(direccion);
     const instance = axios.create({
         baseURL: `https://api.sebastian.cl/paradas/api/v1/direcciones/coordenadas?direccion=${direccion}`,
     })
-
     const respuesta = await instance.get()
+
+    if( respuesta.length === 0){
+        throw new Error(`No hay resultado para ${direccion}`)
+    } else{
     const dir = respuesta.data.direccion;
-    console.log(dir);
-    console.log();
     const lat = respuesta.data.geolocation.latitude;
-    console.log(lat);
-    console.log();
     const long = respuesta.data.geolocation.longitude;
-    console.log(long);
-    console.log();
     return {
         dir,
         lat,
         long
         }
     }
-
-
+}
 
 module.exports = {
     getCoordenadas
