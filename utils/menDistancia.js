ee7b81c34af1ed260f652502fcec540180dc61fd
Daniela Galledi <dangalledi@gmail.com>
2020-01-10 19:20:20 -0300

    CHG: Limpieza de codigo.

--- utils/menDistancia.js
+++ utils/menDistancia.js
@@ -1,25 +1,28 @@
 const fs = require('fs');
+const path = require('path');
 
-//  Requiere el .json con todas las paradas (direccion y latitudes) 
-//const Paradas = require('../data/paradas.json');
+// Menor distancia entre punto de inicio y una parada
+function menDistancia (direccion) {
 
-// Menor distancia entre punto de inicio y una parada 
-const menDistancia = async (direccion) => {
-    const json_paradas = fs.readFileSync('../data/paradas.json', 'utf-8');
+    const json_paradas = fs.readFileSync(path.join(__dirname,'../data/paradas.json' ), 'utf-8');
     let paradas = JSON.parse(json_paradas);
     var menDist = 999;
-    for(var i = 0; i < paradas.length;i++){
-        var latParadero = direccion.latitud- paradas[i].latitud;
-        var longParadero = direccion.longitud - paradas[i].longitud;
+    for(var i = 0; i < paradas.length ;i++){
+        var latParadero = direccion.lat- paradas[i].latitud;
+        var longParadero = direccion.long - paradas[i].longitud;
         var distancia = Math.sqrt( Math.pow(latParadero,2) + Math.pow(longParadero,2));
-    
+
         if (menDist >= distancia) {
             menDist = distancia
             var paraderoCodigo = paradas[i].codigo;
-            var paraderoDireccion = paradas[i].direccion;   
+            var paraderoDireccion = paradas[i].direccion;
         }
-    } return menDist;
-}  
+    } return {
+        menDist,
+        paraderoCodigo,
+        paraderoDireccion
+    };
+}
 
 module.exports = {
     menDistancia
