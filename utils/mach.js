9f7b2cb40e127cc0017777b424b4048da8f00925
Daniela Galledi <dangalledi@gmail.com>
2020-01-10 23:18:43 -0300

    ADD: Se agrega mach directo entre paraderos

---
+++ utils/mach.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+function getBuses (paraderos){
+    
+    const json_paradas = fs.readFileSync(path.join(__dirname,'../data/paradas.json' ), 'utf-8');
+    let paradas = JSON.parse(json_paradas);
+
+    var busesParInicial = [paraderos[0].paraderoBuses,paraderos[0].paraderoBuses1];
+    var busesParFinal = [paraderos[1].paraderoBuses,paraderos[1].paraderoBuses1];
+    
+    console.log(busesParFinal);
+    console.log(busesParInicial);
+
+    for (var i=0; i < busesParInicial.length; i++ ){
+        for (var j=0; j< busesParFinal.length; j++){
+            for(var k=0; k< busesParFinal[i].length; k++){
+                for(var o=0; o< busesParFinal[j].length; o++){
+                    if (busesParInicial[i][k]=== busesParFinal[j][k]){
+                        console.log ('hay un puto mach');
+                        var bus = busesParInicial[i][k];
+                        console.log(bus);
+                        return bus;
+                    }
+                 }
+            
+             }
+         }
+    return 0;
+    }
+}
+
+module.exports = {
+    getBuses
+}
