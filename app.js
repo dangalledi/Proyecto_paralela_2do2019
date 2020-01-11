7db447e8970a8c42522cccf0b78d9aa5fb9236d6
Daniela Galledi <dangalledi@gmail.com>
2020-01-10 23:20:05 -0300

    UP : 1Mach

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
 //npm install 
 //docker build -t node-api .
 //docker run -it -p 9001:3000 -v "$(pwd):/app" node-api
-const express = require("express");
-const app = express();
+//const express = require("express");
+//const app = express();
 //const rp = require('request-promise');
 
 //  Utils
 const get = require('./utils/get');
 const menDistancia = require('./utils/menDistancia');
+const mach = require('./utils/mach');
 
 
 async function getInfo (direccionInicio, direccionDestino){
   try{
-      //Busca en paralelo ambas direcciones las coordenadas respectivas
-      const direcciones = await Promise.all([get.getCoordenadas(direccionInicio), get.getCoordenadas(direccionDestino)]);
+    console.time('Measuring time');
+    //Busca en paralelo ambas direcciones las coordenadas respectivas
+    const direcciones = await Promise.all([get.getCoordenadas(direccionInicio), get.getCoordenadas(direccionDestino)]);
 
-      console.log(direcciones[1].lat);
+    //Busca en paralelo ambas direcciones los paraderos mas cercanos
+    const paraderos = await Promise.all([menDistancia.menDistancia(direcciones[0]), menDistancia.menDistancia(direcciones[1])]);
+    console.log(paraderos);
 
-      //Busca en paralelo ambas direcciones los paraderos mas cercanos
-      //const distancias = await Promise.all([menDistancia.menDistancia(direcciones[0]), menDistancia.menDistancia(direcciones[1])]);
+    //Busca mach de paraderos segun las micros 
+    const buses = await mach.getBuses(paraderos);
 
-      //Busca mach de paraderos segun las micros 
-
-
-    } catch (e) {
-      console.log(e);
-      console.log(`No se pudo obtener informacion de ${direccionInicio} y ${direccionDestino}`)
-    }
+    console.timeEnd('Measuring time')
+  } catch (e) {
+    console.log(e);
+    console.log(`No se pudo obtener informacion de ${direccionInicio} y ${direccionDestino}`)
+  }
 }
 
-getInfo('palestrina 2710, san joaquin', '1 de mayo 4205, pedro aguirre cerda');
+getInfo('palestrina 2710, san joaquin', 'Gral Rondizzoni 1670,santiago');
 
 /*// settings
 const port = process.env.PORT || 3000;
