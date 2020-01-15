480afc35571a37e29d198350d9e53917a7ef8eb0
Daniela Galledi <dangalledi@gmail.com>
2020-01-14 22:43:36 -0300

    ADD: Se agrego GET para diferenciar consulta y CHG: Server

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //npm install
 //docker build -t node-api .
-//docker run --rm -it -p 9001:3000 -v "$(pwd):/app" node-api
+//docker run --rm -it -p 3000:3000 -v "$(pwd):/app" node-api
 const express = require("express");
+var cors = require('cors');
 const morgan = require('morgan');
 const app = express();
 const path = require('path');
@@ -11,10 +12,11 @@ const info = require(path.join(__dirname,'./utils/info'));
 app.set('port', process.env.PORT || 3000);
 
 // middlewares
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+app.use(cors())
 
 // routes
-app.get('/:dirInicio/:dirDestino', async (req, res) => {
+app.get('/dir/:dirInicio/:dirDestino', async (req, res, next) => {
 
     const direccionInicio = req.params.dirInicio +',santiago,chile';
     const direccionDestino= req.params.dirDestino +',santiago,chile';
@@ -33,12 +35,35 @@ app.get('/:dirInicio/:dirDestino', async (req, res) => {
     }
 });
 
-app.get('/hola', function (req, res) {
+app.get('/coor/:coorInicio/:dirDestino', async (req, res, next) => {
+
+  var coorInicio = req.body.coorInicio;
+  const direccionDestino= req.params.dirDestino +',santiago,chile';
+
+  if (coorInicio && direccionDestino ) {
+    try{
+      var recorrido = await info.getInfoCoor(coorInicio, direccionDestino);
+      console.log(recorrido);
+      res.json(recorrido);
+    }catch(e){
+      console.log(e);
+    }
+    
+    } else {
+      res.status(500).json({error: 'No se han ingresado las variables'});
+      console.log(e);
+  }
+});
+
+app.get('/', function (req, res, next) {
    res.send('Holi');
 });
 
 
 // Starting the server
-app.listen(app.get('port'), () => {
-	console.log(`Server on port ${app.get('port')}`);
-});
+var server = app.listen(app.get('port'), function () {
+  var host = server.address().address
+  var port = server.address().port
+  
+  console.log("Server listening at http://%s:%s", host, port)
+})
